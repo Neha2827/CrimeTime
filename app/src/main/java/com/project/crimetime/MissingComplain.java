514426e7d4680778afebfcb955c78ab7383cf8c4
Ayushmina <ayushminanayak20@gmail.com>
2020-09-22 22:28:21 -0700

    added admin to firestore

--- app/src/main/java/com/project/crimetime/MissingComplain.java
+++ app/src/main/java/com/project/crimetime/MissingComplain.java
@@ -11,25 +11,47 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MissingComplain extends AppCompatActivity {
-    Button photo;
+    ImageView imageView;
+    EditText mEtmissing_name,mEtage,mEtheight,mEtmissing_skin,mEthair,mEtmissing_time,mEtmissing_place;
+    EditText mEtcomplainer_name,mEtphone,mEtaddress,mEtpincode,mEtdate;
     Button launch;
+    FirebaseFirestore firebaseFirestore;
+    FirebaseAuth firebaseAuth;
+    String id;
+    CollectionReference collectionReference=firebaseFirestore.collection("missing complaints").
+            document(id).collection("missing complaint details");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_missing_complain);
-
+        imageView=findViewById(R.id.img_view);
+        mEtmissing_name=findViewById(R.id.missing_name);
+        mEtage=findViewById(R.id.age);
+        mEtheight=findViewById(R.id.height);
+        mEtmissing_skin=findViewById(R.id.missing_skin);
+        mEthair=findViewById(R.id.hair);
+        mEtmissing_place=findViewById(R.id.missing_place);
+        mEtmissing_time=findViewById(R.id.missing_time);
+        mEtcomplainer_name=findViewById(R.id.complainer_name);
+        mEtphone=findViewById(R.id.complainer_phone);
+        mEtaddress=findViewById(R.id.complainer_address);
+        mEtpincode=findViewById(R.id.pincode);
+        mEtdate=findViewById(R.id.date);
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
         launch = findViewById(R.id.launch_btn);
-        photo = findViewById(R.id.photo);
 
-        photo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MissingComplain.this, Camera.class);
-                startActivity(intent);
-            }
-        });
+
+
 
         launch.setOnClickListener(new View.OnClickListener() {
             @Override
