d676ea4bd657b7ead4738b68e9d4007f1f0b6601
Aditya <adityabansal264@gmail.com>
2020-10-16 12:26:35 +0530

    Splash Screen

---
+++ app/src/main/java/com/project/crimetime/Splash.java
@@ -0,0 +1,36 @@
+package com.project.crimetime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN=2500;
+    TextView mTvCrimeTime;
+    Animation animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mTvCrimeTime=findViewById(R.id.tv_crimeTime);
+        animation= AnimationUtils.loadAnimation(this,R.anim.rtl);
+        mTvCrimeTime.startAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
