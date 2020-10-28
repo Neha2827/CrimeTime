4798dfd81471aaef7d5cc87ef5953d748bcf06a7
Aditya <adityabansal264@gmail.com>
2020-10-28 11:20:23 +0530

    User and Admin updated

--- app/src/main/java/com/project/crimetime/AdminHome.java
+++ app/src/main/java/com/project/crimetime/AdminHome.java
@@ -1,5 +1,6 @@
 package com.project.crimetime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,7 +8,13 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.project.crimetime.Fragments.AdminLogin;
 import com.project.crimetime.Fragments.UserLogin;
 
@@ -16,21 +23,42 @@ public class AdminHome extends AppCompatActivity {
     Button complain;
     private SharedPreferences prefManager;
     private SharedPreferences.Editor editor;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    String string;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
 
         complain = findViewById(R.id.admin_complain);
         prefManager = getSharedPreferences("APP", 0);
         editor = prefManager.edit();
+        string=firebaseAuth.getCurrentUser().getUid();
 
 
         complain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(AdminHome.this, AdminComplaints.class);
-                startActivity(intent);
+              firebaseFirestore.collection("admin").document(string).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                  @Override
+                  public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                      if (task.getResult().exists()){
+                          Intent intent = new Intent(AdminHome.this, AdminComplaints.class);
+                          startActivity(intent);
+                      }
+                      else {
+                          Toast.makeText(AdminHome.this, "Not Registered as Admin", Toast.LENGTH_LONG).show();
+
+                          Intent back=new Intent(AdminHome.this,LogIn.class);
+                          startActivity(back);
+                          finish();
+                      }
+
+                  }
+              });
             }
         });
     }
--- app/src/main/java/com/project/crimetime/HomeScreen.java
+++ app/src/main/java/com/project/crimetime/HomeScreen.java
@@ -1,5 +1,6 @@
 package com.project.crimetime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,7 +8,13 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.project.crimetime.Fragments.UserLogin;
 
 public class HomeScreen extends AppCompatActivity {
@@ -15,10 +22,15 @@ public class HomeScreen extends AppCompatActivity {
     Button launchComplain, myComplaints,mymissingComplaints,crimeReport;
     private SharedPreferences prefManager;
     private SharedPreferences.Editor editor;
+    FirebaseFirestore firebaseFirestore;
+    FirebaseAuth firebaseAuth;
+    String string;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        firebaseAuth=FirebaseAuth.getInstance();
         mymissingComplaints=findViewById(R.id.missing_complain);
 
         launchComplain = findViewById(R.id.launch_complain);
@@ -26,6 +38,7 @@ public class HomeScreen extends AppCompatActivity {
         crimeReport = findViewById(R.id.report);
         prefManager = getSharedPreferences("APP", 0);
         editor = prefManager.edit();
+        string=firebaseAuth.getCurrentUser().getUid();
 
 
        // String userName = prefManager.getString(UserLogin.KEY_EMAIL_ADDRESS, "No Value available");
@@ -33,24 +46,69 @@ public class HomeScreen extends AppCompatActivity {
         mymissingComplaints.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(HomeScreen.this,MyMissing.class);
-                startActivity(intent);
+               firebaseFirestore.collection("users").document(string).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                   @Override
+                   public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                       if (task.getResult().exists()){
+                           Intent intent=new Intent(HomeScreen.this,MyMissing.class);
+                           startActivity(intent);
+                       }else {
+                           Toast.makeText(HomeScreen.this, "Not Registered as User", Toast.LENGTH_LONG).show();
+                           Intent back=new Intent(HomeScreen.this,LogIn.class);
+                           startActivity(back);
+                           finish();
+
+                       }
+
+                   }
+               });
             }
         });
 
         launchComplain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeScreen.this,LaunchComplain.class);
-                startActivity(intent);
+              firebaseFirestore.collection("users").document(string).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                  @Override
+                  public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                      if (task.getResult().exists()){
+                          Intent intent = new Intent(HomeScreen.this,LaunchComplain.class);
+                          startActivity(intent);
+                      }
+                      else {
+                          Toast.makeText(HomeScreen.this, "Not Registered as User", Toast.LENGTH_LONG).show();
+
+                          Intent back=new Intent(HomeScreen.this,LogIn.class);
+                          startActivity(back);
+                          finish();
+
+
+                      }
+
+                  }
+              });
             }
         });
 
         myComplaints.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeScreen.this,MyComplaints.class);
-                startActivity(intent);
+               firebaseFirestore.collection("users").document(string).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                   @Override
+                   public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                       if (task.getResult().exists()){
+                           Intent intent = new Intent(HomeScreen.this,MyComplaints.class);
+                           startActivity(intent);
+                       }else {
+                           Toast.makeText(HomeScreen.this, "Not Registered as User", Toast.LENGTH_LONG).show();
+
+                           Intent back=new Intent(HomeScreen.this,LogIn.class);
+                           startActivity(back);
+                           finish();
+                       }
+
+                   }
+               });
             }
         });
 
